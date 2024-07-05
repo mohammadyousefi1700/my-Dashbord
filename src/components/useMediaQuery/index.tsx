86f13d5e99a751c3e0be55fa5a091447feacb5ad
m.yousefi <mohammad.yousefi.1700@gmail.com>
2024-07-06 02:19:55 +0330

    .

---
+++ src/components/useMediaQuery/index.tsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+
+function useMediaQuery(query: string) {
+  const [matches, setMatches] = useState(false);
+
+  useEffect(() => {
+    const mediaQueryList = window.matchMedia(query);
+    const documentChangeHandler = () => setMatches(mediaQueryList.matches);
+
+    documentChangeHandler(); // Set the initial value
+    mediaQueryList.addEventListener("change", documentChangeHandler);
+
+    return () => {
+      mediaQueryList.removeEventListener("change", documentChangeHandler);
+    };
+  }, [query]);
+
+  return matches;
+}
+
+export default useMediaQuery;
